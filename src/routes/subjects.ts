cede689dea610e60efc090d63a680fab740adf0c
Diwakar Singh <diwakarsi9090@gmail.com>
2026-04-18 03:04:48 +0530

    Added file extentions to .js

--- src/routes/subjects.ts
+++ src/routes/subjects.ts
@@ -1,6 +1,6 @@
 import { and, desc, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
-import { departments, subjects } from "../db/schema";
-import { db } from "../db";
+import { departments, subjects } from "../db/schema/index.js";
+import { db } from "../db/index.js";
 
 
 import express from 'express';
