55437d0f8a7a06290b091f45877d66a6ab6dd991
Diwakar Singh <diwakarsi9090@gmail.com>
2026-04-17 22:19:11 +0530

    Implemented arcjet as middleware for security

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import subjectsRouter from './routes/subjects';
+import securityMiddleware from './middleware/security';
 
 const app = express();
 const PORT = 8000;
@@ -19,6 +20,7 @@ app.use(cors({
 
 // Middleware
 app.use(express.json());
+app.use(securityMiddleware)
 
 app.use('/api/subjects', subjectsRouter);
 
