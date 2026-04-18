108c4e562b6f4d9da5bf994dce5095f3a5860ca4
Diwakar Singh <diwakarsi9090@gmail.com>
2026-04-18 17:28:10 +0530

    fixes orders2

--- src/index.ts
+++ src/index.ts
@@ -4,42 +4,41 @@ AgentAPI.config()
 import express from 'express';
 import cors from 'cors';
 import subjectsRouter from './routes/subjects.js';
-import securityMiddleware from './middleware/security.js';
+// import securityMiddleware from './middleware/security.js';
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth.js';
 
 const app = express();
 // 🔥 REQUIRED for Railway
 app.set("trust proxy", 1);
-const PORT = Number(process.env.PORT ?? 8000);
-const HOST = process.env.HOST ?? '0.0.0.0';
+const PORT = 8000;
 
+
+// if (!process.env.FRONTEND_URL) {
+    //   throw new Error('FRONTEND_URL is required');
+    // }
+    
 //Cors configuration
-
-if (!process.env.FRONTEND_URL) {
-  throw new Error('FRONTEND_URL is required');
-}
-
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
-}))
-
+}));
 
 // Middleware
-app.use(express.json());
 // app.use(securityMiddleware)
 app.all('/api/auth/{*splat}', toNodeHandler(auth));
+
+app.use(express.json());
 
 app.use('/api/subjects', subjectsRouter);
 
 // Root GET route
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the Classroom Backend API' });
+app.get("/", (req, res) => {
+  res.send("Backend server is running!");
 });
 
 // Start server
-app.listen(PORT, HOST, () => {
-    console.log(`Server is running on ${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
