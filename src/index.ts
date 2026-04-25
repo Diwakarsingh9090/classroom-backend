6d3c8f447226e207ad3b0e23eafbc8ee58a9df1d
Diwakar Singh <diwakarsi9090@gmail.com>
2026-04-26 01:20:11 +0530

    Fixes some router issues

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import cors from 'cors';
 import subjectsRouter from './routes/subjects.js';
 import usersRouter from './routes/users.js';
 import classesRouter from './routes/classes.js';
+import departmentsRouter from "./routes/departments.js";
+import statsRouter from "./routes/stats.js";
+import enrollmentsRouter from "./routes/enrollments.js";
 // import securityMiddleware from './middleware/security.js';
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth.js';
@@ -37,6 +40,9 @@ app.use(express.json());
 app.use('/api/subjects', subjectsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/classes', classesRouter);
+app.use("/api/departments", departmentsRouter);
+app.use("/api/stats", statsRouter);
+app.use("/api/enrollments", enrollmentsRouter);
 
 
 // Root GET route
